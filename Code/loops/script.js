e0a859e2c43091d46794a6522087cac913de0512
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-11 19:59:44 +0500

    For Loop

---
+++ Code/loops/script.js
@@ -0,0 +1,19 @@
+
+// let numbers = [1,3,5,6,7,8,19];
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// console.log(numbers[0])
+// console.log(numbers[1])
+// console.log(numbers[2])
+
+// for(let i = 0; i < numbers.length; i++ ){
+//     console.log(numbers[i])
+// }
+
+// let i = 3;
+// for(; i < numbers.length;  ){
+//     console.log(numbers[i])
+//     i++;
+// }
