acea55adc1f8057e3cfc84757417ae736c7c891e
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-20 16:35:17 +0500

    functions

---
+++ Code/functions/script.js
@@ -0,0 +1,64 @@
+let userName = "Arif";
+let lastName = "Khan";
+
+
+
+
+// console.log(`Welcome ${userName} ${lastName}! `);
+
+
+// let num1 = 10;
+// let num2 = 20;
+
+
+
+// function sum()
+// {
+
+
+//     console.log(num1 + num2)
+// }
+// function sum(num1, num2)
+// {
+
+
+//     console.log(num1 + num2)
+// }
+
+// sum();
+
+// num1 = 90;
+// num2 = 70;
+
+
+// sum();
+
+// num1 = 10;
+// num2 = 20;
+
+// sum();
+
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+
+function print(g) {
+    for (let i = 0; i < g.length; i++) {
+        console.log(g[i]);
+    }
+}
+
+
+
+print(array);
+
+
+let names = ['Arif','Zahid','Ajmal','Anas'];
+
+
+print(names)
+
+
+
