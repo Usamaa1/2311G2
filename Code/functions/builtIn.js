10eb9dc0767d080389a22ba8705c0b1e7f8fc8bf
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-20 17:58:19 +0500

    Date, String, Numbers and Math builtIn Functions completed!

---
+++ Code/functions/builtIn.js
@@ -0,0 +1,95 @@
+// Math & Numbers functions
+
+
+// console.log(Number.isFinite("fjdhsajfh"))
+// console.log(Number.isInteger(23))
+// console.log(Number.isInteger(23.3))
+// console.log(isNaN("fdsfds"))
+// console.log(isNaN(4))
+
+// let a = 23.9988777;
+
+// console.log(a.toFixed(2));
+// console.log(a.toPrecision());
+
+// Math Methods
+
+// console.log(Math.max(2,3,4,56,7,23,45,21))
+// console.log(Math.min(2,3,4,56,7,23,45,21))
+// console.log(Math.sqrt(144));
+// console.log(Math.cbrt(283));
+// console.log(Math.pow(3,2))
+// console.log(Math.pow(3,3))
+// console.log(Math.PI)
+// console.log(Math.trunc(Math.random() * 10 + 1))
+// console.log(-23)
+// console.log(Math.abs(-23))
+// console.log(Math.ceil(1.555))
+// console.log(Math.floor(1.999955))
+// console.log(Math.trunc(13.899999))
+
+
+
+// **************************************
+// **************************************
+// ********** Date Methods **************
+// **************************************
+// **************************************
+
+
+// let currentDate = new Date();
+
+
+// console.log(currentDate)
+// console.log(currentDate.getDate())
+// console.log(currentDate.getDay())
+// console.log(currentDate.getFullYear())
+// console.log(currentDate.getHours())
+// console.log(currentDate.getSeconds())
+// console.log(currentDate.getMinutes())
+// console.log(currentDate.getMilliseconds())
+// console.log(currentDate.getTime())
+
+// Sunday ---> 0
+// Monday ---> 1
+// Tuesday ---> 2
+// Wednesday ---> 3
+
+
+// **************************************
+// **************************************
+// ********** String Methods **************
+// **************************************
+// **************************************
+
+
+// let str1 = "This is string";
+// let str2 = "This is my second string";
+// let num = 23;
+
+// let numString = num.toString();
+
+// length
+// toString
+// toLowerCase
+// toUperCase
+// search
+// replace
+// typeOf
+
+
+// console.log(str1.length);
+// console.log(str1.toUpperCase())
+// console.log(str1.toLowerCase())
+// console.log(typeof(num))
+// console.log(typeof(numString))
+// console.log(typeof(num.toString()))
+// console.log(num.toString())
+// console.log(str2.search("f"))
+// console.log(str2.replace("This","That"))
+// console.log(str2.replace(/ is/g," ip"))
+// console.log()
+
+
+
+
