1682606fb9028c3850830e71a914602f9f950e20
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-22 17:55:14 +0500

    Objects and array of objects completed

---
+++ Code/objects/script.js
@@ -0,0 +1,45 @@
+// const person = 
+// {
+//     firstName: "Owais",
+//     lastName: "Ahmed",
+//     age: 56,
+//     job: "Software Developer",
+//     favColor: ['blue','black','purple']
+// }
+
+// console.log(person)
+
+// console.log(person.firstName)
+
+// console.log(person.age)
+// console.log(person.favColor);
+// console.log(person.favColor[1])
+
+
+// Array of Objects
+
+// const emps = [
+//     {
+//             firstName: "Owais",
+//             lastName: "Ahmed",
+//             age: 56,
+//             job: "Software Developer",
+//             favColor: ['blue','black','purple']
+//         },
+//         {
+//                 firstName: "Haris",
+//                 lastName: "zahid",
+//                 age: 16,
+//                 job: "Accountant",
+//                 favColor: ['Magenta','Gray','White']
+//             }
+// ]
+
+
+// console.log(emps)
+
+// for (let i = 0; i < emps.length; i++) {
+//     console.log(emps[i].age)
+    
+// }
+
