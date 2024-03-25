98d2b7ef0f86e274e23ff5e0801f8bd6f6944fbb
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-25 17:57:04 +0500

    DOM completed only events remaining

---
+++ Code/DOM/script.js
@@ -0,0 +1,48 @@
+
+// let p1 = document.getElementById('p1');
+// let p = document.getElementsByClassName("p");
+// let pTag = document.getElementsByTagName("p");
+
+// let p = document.querySelector("p");
+// let p = document.querySelector("#p1");
+// let p = document.querySelectorAll(".p");
+// let p = document.querySelectorAll("p");
+
+
+// console.log(p[3])
+
+// console.log(pTag[1].innerText)
+// console.log(pTag[0].innerText, pTag[1].innerText)
+
+// console.log(p)
+
+
+
+// let d = document.querySelector('.d');
+
+// console.log(d.innerText)
+// console.log(d.innerHTML)
+
+// divP.innerText = "jfksdajfkjsdkjf"
+
+
+
+// let divP = document.getElementById('divP');
+
+
+// divP.innerHTML = "<h1>Hello this text is from Javascript</h1> hfjsdhfjhjsdhfjhsdhfjh"
+
+// divP.innerHTML = "";
+
+// let span = document.createElement('div');
+// span.innerText = "This is span tag";
+// divP.appendChild(span)
+
+
+// img.src = "1.jpg";
+
+// a.style.color = "grey";
+// a.style.backgroundColor = "yellow";
+// a.style.display = "none"
+
+// a.setAttribute("class","abc")
