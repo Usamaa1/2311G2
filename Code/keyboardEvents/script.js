b3c3e69118c6518a927cb6513165ea7e045b4c93
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-04-08 17:59:07 +0500

    Regex Completed

--- Code/keyboardEvents/script.js
+++ Code/keyboardEvents/script.js
@@ -75,13 +75,44 @@ zip.addEventListener('keyup',()=>{
         zipError.innerHTML = "Kindly Enter Numbers only"
     }
 
+})
 
+email.addEventListener('keyup',()=>{
+    console.log(email.value);
 
+    let emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*.(\w{2,4})$/g;
+
+    if(emailRegEx.test(email.value))
+    {
+        emailError.innerHTML ="";
+    }
+    else
+    {
+        emailError.innerHTML = "Kindly enter valid email only"
+    }
 
 })
 
 
+password.addEventListener('keyup',()=>{
+    console.log(password.value);
+
+    let passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/g;
+
+    if(passwordRegEx.test(password.value))
+    {
+        passError.innerHTML ="";
+    }
+    else
+    {
+        passError.innerHTML = "Password must contains 1 uppercase 1 lower case Letter, 1 special Character  and 1 Number only"
+    }
+
+})
 
 
+eye.addEventListener('click',()=>{
+    password.setAttribute('type','text')
+})
 
 
